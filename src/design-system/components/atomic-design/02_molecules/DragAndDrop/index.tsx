289e5a11ff9f512e605f0a1a05240863bafd19cc
andradeviniicius <viniciusdandrade01@gmail.com>
2022-09-29 16:09:57 -0300

    feat: Create FileManagement w add&remove functions

--- src/design-system/components/atomic-design/02_molecules/DragAndDrop/index.tsx
+++ src/design-system/components/atomic-design/02_molecules/DragAndDrop/index.tsx
@@ -1,32 +1,56 @@
-import * as S from './styles'
-import { Props } from './types'
-import {useDropzone} from 'react-dropzone'
-import {useCallback, useState} from 'react'
+import * as S from "./styles";
+import { Props } from "./types";
+import { useDropzone } from "react-dropzone";
+import { useCallback, useState } from "react";
 
-import { Description } from '../../01_atoms/Description';
-import { Button } from '../../01_atoms/Button';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCloudArrowUp } from '@fortawesome/free-solid-svg-icons'
+import { Description, Button } from "../../01_atoms";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCloudArrowUp, faX } from "@fortawesome/free-solid-svg-icons";
 
-export function DragAndDrop({descriptionText, buttonText}: Props) {
+export interface UploadableFile {
+  file: File;
+}
+
+export function DragAndDrop({ descriptionText, buttonText }: Props) {
+  const [files, setFiles] = useState<UploadableFile[]>([]);
+
+  const removeCurrentFile = (event: any, file: File) => {
+    event.preventDefault();
+    setFiles((curr) => curr.filter((allFiles) => allFiles.file !== file));
+  };
+
   const onDrop = useCallback((acceptedFiles: File[]) => {
-    console.log(acceptedFiles[0]);
-  }, [])
-  const {getRootProps, getInputProps, isDragActive, acceptedFiles} = useDropzone({onDrop, maxFiles: 1})
+    const mappedFiles = acceptedFiles.map((file) => ({ file }));
+    setFiles((curr) => [...curr, ...mappedFiles]);
+  }, []);
+
+  const { getRootProps, getInputProps, isDragActive, acceptedFiles } =
+    useDropzone({ onDrop });
 
   return (
-    <S.DragAndDrop {...getRootProps()} className="drag-n-drop" isActive={isDragActive}>
-      <input {...getInputProps()} />
-      <FontAwesomeIcon
-        className="font-awesome-icon"
-        icon={faCloudArrowUp}
-      />
-      <Description
-        descriptionText={descriptionText}
-      />
-      <Button
-        buttonText={buttonText}
-      />
-    </S.DragAndDrop>
-  )
+    <>
+      <S.DragAndDrop
+        {...getRootProps()}
+        className="drag-n-drop"
+        isActive={isDragActive}
+      >
+        <input {...getInputProps()} />
+        <FontAwesomeIcon className="font-awesome-icon" icon={faCloudArrowUp} />
+        <Description descriptionText={descriptionText} />
+        <Button buttonText={buttonText} />
+      </S.DragAndDrop>
+
+      <S.UlSelectedFiles>
+        {files.map((file, index) => (
+          <li className="" key={index}>
+            <button onClick={(event) => removeCurrentFile(event, file.file)}>
+              <FontAwesomeIcon className="font-awesome-icon" icon={faX} />
+            </button>
+            {file.file.name} - {file.file.size} bytes
+          </li>
+        ))}
+      </S.UlSelectedFiles>
+    
+    </>
+  );
 }
