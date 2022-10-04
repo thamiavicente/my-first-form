81dea295b3479c499d9aa0d48d85ea0320e5599c
andradeviniicius <viniciusdandrade01@gmail.com>
2022-10-04 14:19:31 -0300

    fix/replace <SideBySide/>  w/  <Field/> using grid

--- src/design-system/components/atomic-design/05_pages/Form/styles.ts
+++ src/design-system/components/atomic-design/05_pages/Form/styles.ts
@@ -1,23 +1,40 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
-export const Form = styled.form `
+export const Form = styled.form`
   @media (min-width: 1024px) {
     width: 80%;
     max-width: 768px;
   }
 
-  width: 90%;
-  display: flex;
-  justify-content: center;
-  flex-direction: column;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-template-rows: repeat(3, 100px);
+  grid-column-gap: 20px;
   margin: auto;
+  width: 90%;
 
   .button {
     @media (min-width: 1024px) {
       width: 24%;
       align-self: flex-end;
     }
-    
+
+    grid-column-start: 1;
+    grid-column-end: 3;
     width: 100%;
   }
-`
+
+  div {
+    grid-column-start: 1;
+    grid-column-end: 3;
+  }
+
+  div:nth-child(2) {
+    grid-column-start: 1;
+    grid-column-end: 2;
+  }
+  div:nth-child(3) {
+    grid-column-start: 2;
+    grid-column-end: 3;
+  }
+`;
